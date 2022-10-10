241cca773990f5d4eebbeee03f88b5cccb451743
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-10-10 20:34:55 +0900

    10.10

---
+++ chap03/src/ch02/Customer.java
@@ -0,0 +1,13 @@
+package ch02;
+
+public class Customer {
+    public int customerId;
+    public String customerName;
+    public String customerGrade;
+    int bonusPoint;
+    double bonusRatio;
+
+    public Customer() {
+        bonusRatio = 0.01;
+    }
+}
