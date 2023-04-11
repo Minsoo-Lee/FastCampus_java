fd4d92cae9d5067ff36e577fc461a1a6ef31c6c9
Minsoo-Lee <minsoo.james1101@gmail.com>
2023-04-12 03:03:52 +0900

    Filter-Interceptor의 활용 (1)

---
+++ Part3/filter/src/main/java/com/example/filter/dto/User.java
@@ -0,0 +1,12 @@
+package com.example.filter.dto;
+
+import lombok.*;
+
+@Getter @Setter @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private String name;
+    private int age;
+}
